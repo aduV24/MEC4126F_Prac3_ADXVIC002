ca0e589059ed8ae39c08592a51dc605f6852036e
Victor Adu <58883511+aduV24@users.noreply.github.com>
2023-03-06 15:29:36 +0200

    Q3.2
    Displayed age on LCD using a for loop

--- Debug/src/subdir.mk
+++ Debug/src/subdir.mk
@@ -10,9 +10,6 @@ C_SRCS += \
 ../src/stm32f0xx_hal_msp.c \
 ../src/write.c 
 
-CPP_SRCS += \
-../src/main.cpp 
-
 OBJS += \
 ./src/initialize-hardware.o \
 ./src/lcd_stm32f0.o \
@@ -27,22 +24,12 @@ C_DEPS += \
 ./src/stm32f0xx_hal_msp.d \
 ./src/write.d 
 
-CPP_DEPS += \
-./src/main.d 
-
 
 # Each subdirectory must supply rules for building sources it contributes
 src/%.o: ../src/%.c src/subdir.mk
 	@echo 'Building file: $<'
 	@echo 'Invoking: GNU Arm Cross C Compiler'
 	arm-none-eabi-gcc -mcpu=cortex-m0 -mthumb -Og -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -ffreestanding -fno-move-loop-invariants -Wall -Wextra -g3 -DDEBUG -DUSE_FULL_ASSERT -DTRACE -DOS_USE_TRACE_SEMIHOSTING_DEBUG -DSTM32F051x8 -DUSE_HAL_DRIVER -DHSE_VALUE=48000000 -I"../include" -I"../system/include" -I"../system/include/cmsis" -I"../system/include/stm32f0-hal" -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
-	@echo 'Finished building: $<'
-	@echo ' '
-
-src/%.o: ../src/%.cpp src/subdir.mk
-	@echo 'Building file: $<'
-	@echo 'Invoking: GNU Arm Cross C++ Compiler'
-	arm-none-eabi-g++ -mcpu=cortex-m0 -mthumb -Og -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -ffreestanding -fno-move-loop-invariants -Wall -Wextra -g3 -DDEBUG -DUSE_FULL_ASSERT -DTRACE -DOS_USE_TRACE_SEMIHOSTING_DEBUG -DSTM32F051x8 -DUSE_HAL_DRIVER -DHSE_VALUE=48000000 -I"../include" -I"../system/include" -I"../system/include/cmsis" -I"../system/include/stm32f0-hal" -std=gnu++11 -fabi-version=0 -fno-exceptions -fno-rtti -fno-use-cxa-atexit -fno-threadsafe-statics -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
--- src/main.c
+++ src/main.c
@@ -7,6 +7,7 @@
 #define STM32F051												   //COMPULSORY
 #include "stm32f0xx.h"											   //COMPULSORY
 #include "lcd_stm32f0.h"
+#include <stdio.h>
 
 // GLOBAL VARIABLES ----------------------------------------------------------|
 struct age_data{
@@ -25,11 +26,23 @@ void main(void);                                                   //COMPULSORY
 
 void main(void)
 {
-
+	my_age.Day =24;
+	my_age.month = 5;
+	my_age.year = 2000;
+	my_age.age = 22;
+	char str[2]; // Buffer array that stores the string
+	init_LCD(); // initialize LED
 
 	while(1)
 	{
 
+		for (uint8_t var = 0; var <= my_age.age; var++) {
+			delay(65000);
+			sprintf(str,"%d",var); //write age into str array
+			lcd_putstring(str); // put string on lcd
+			delay(65000);
+			lcd_command(CLEAR); //clear screen for next number
+		}
 	}
 }
 
